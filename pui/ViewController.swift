47bf4b4853bdbd0bfa4d8f24bb629cf723f5ebad
alieris <v9zz@hotmail.com>
2023-01-10 19:18:44 +0300

    aa

--- pui/ViewController.swift
+++ pui/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import PencilKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PKCanvasViewDelegate {
 
+    
+    let myCanvas = PKCanvasView()
+    let toolPicker = PKToolPicker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        myCanvas.delegate = self
+        
+        view.addSubview(myCanvas)
+        myCanvas.drawingPolicy = .anyInput
+      
+        toolPicker.setVisible(true, forFirstResponder: myCanvas)
+        toolPicker.addObserver(myCanvas)
+        myCanvas.becomeFirstResponder()
+        
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        myCanvas.frame = view.bounds
+    }
+    
 
 }
-
